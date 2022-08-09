d1e8a1f433c28883bf6e5213fd05c6840ef65ce6
Przemysław Rybarczyk <przemyslaw.rybarczyk@protonmail.com>
2022-08-09 11:18:43 +0200

    Added watchdog thread

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <errno.h>
 #include <pthread.h>
+#include <stdatomic.h>
 #include <stdio.h>
 
 #include "reader.h"
@@ -21,11 +22,17 @@ static struct Buffer *core_usage_buffer;
 static pthread_t reader_thread;
 static pthread_t analyzer_thread;
 static pthread_t printer_thread;
+static pthread_t watchdog_thread;
 static pthread_t logger_thread;
+
+static atomic_bool reader_alive = false;
+static atomic_bool analyzer_alive = false;
+static atomic_bool printer_alive = false;
 
 static void *reader_main(void *arg) {
     FILE *file = (FILE *)arg;
     while (1) {
+        reader_alive = true;
         log_msg("Reader: reading /proc/stat");
         struct String *str = read_file(file);
         if (str == NULL) {
@@ -55,6 +62,7 @@ static void *analyzer_main(void *arg) {
     if (prev_cpu_time == NULL)
         return NULL;
     while (1) {
+        analyzer_alive = true;
         struct CPUTime *cpu_time = analyzer_get_cpu_time();
         if (cpu_time == NULL)
             return NULL;
@@ -72,11 +80,37 @@ static void *analyzer_main(void *arg) {
 
 static void *printer_main(void *arg) {
     while (1) {
+        printer_alive = true;
         log_msg("Printer: getting usage");
         double *usage = (double *)buffer_pop(core_usage_buffer);
         log_msg("Printer: displaying usage");
         print_cpu_usage(usage, num_cores);
         free(usage);
+    }
+}
+
+static void *watchdog_main(void *arg) {
+    while (1) {
+        sleep(2);
+        log_msg("Watchdog: running check");
+        bool kill_process = false;
+        if (!reader_alive) {
+            log_msg("Watchdog: no message from reader");
+            kill_process = true;
+        }
+        if (!analyzer_alive) {
+            log_msg("Watchdog: no message from analyzer");
+            kill_process = true;
+        }
+        if (!printer_alive) {
+            log_msg("Watchdog: no message from printer");
+            kill_process = true;
+        }
+        if (kill_process)
+            exit(1);
+        reader_alive = false;
+        analyzer_alive = false;
+        printer_alive = false;
     }
 }
 
@@ -97,10 +131,11 @@ int main() {
     int reader_create_err = pthread_create(&reader_thread, NULL, reader_main, (void *)file);
     int analyzer_create_err = pthread_create(&analyzer_thread, NULL, analyzer_main, NULL);
     int printer_create_err = pthread_create(&printer_thread, NULL, printer_main, NULL);
+    int watchdog_create_err = pthread_create(&watchdog_thread, NULL, watchdog_main, NULL);
     int logger_create_err = pthread_create(&logger_thread, NULL, logger_main, NULL);
-    if (reader_create_err != 0 || analyzer_create_err != 0 || printer_create_err != 0 || logger_create_err != 0) {
+    if (reader_create_err || analyzer_create_err || printer_create_err || watchdog_create_err || logger_create_err) {
         fprintf(stderr, "Error: failed to create thread\n");
-        return printer_create_err;
+        return 1;
     }
     log_msg("Main thread: Program initialization finished");
     pthread_exit(NULL);
